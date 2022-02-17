5f95323bc5f5dc247e198da7f9cc1fbd246e5009
Falller <marlon.emanutec@gmail.com>
2022-02-16 21:52:45 -0300

    envio dos exercícios aula 16/02

---
+++ DESAFIO FAIXA ETÁRIA/script.js
@@ -0,0 +1,24 @@
+console.log("=== ANALISAR FAIXA ETÁRIA")
+
+function analisar() {
+
+    const idade = parseFloat(document.getElementById("idade").value);
+
+    if (idade < 12) {
+        document.getElementById("resposta").value = "Você é uma criança!";
+    } else if (idade >= 12 && idade < 18){
+        document.getElementById("resposta").value = "Você é um Adolescente"
+    } else if (idade >= 18 && idade < 65){
+        document.getElementById("resposta").value = "Você é um Adulto!";
+    } else if (idade >=65 && idade < 99){
+        document.getElementById("resposta").value = "Você é um Idoso!";
+    } else {
+        document.getElementById("resposta").value = "Funerária Já passou da hora, bom dia!";
+    }
+    
+}
+
+function zerar() {
+    document.getElementById("idade").value = "";
+    document.getElementById("resposta").value = "";
+}
--- SEM HTML/condicionais.js
+++ SEM HTML/condicionais.js
@@ -1,2 +1,103 @@
 console.log("=== CONDICIONAIS ===");
 
+function verificaif() {
+//entrada
+    const aluno = document.getElementById("aluno").value;
+    const nota = parseFloat(document.getElementById("nota").value);
+
+    console.log(aluno, nota);
+
+    //processamento
+
+    let resp = null;
+
+    if (nota >= 6) {
+        resp = "Aprovado!"
+    } 
+    
+    if (nota < 6 && nota >=4) {
+        resp = "Recuperação!"
+    }
+
+    if (nota < 4) {
+        resp = "Reprovado!"
+    }
+
+    //saída
+    document.write(
+        `Exemplo feito com IF <br>
+        Aluno: ${aluno} <br>
+        Nota: ${nota} <br>
+        Situação: ${resp}`);
+}
+
+function verificaifelse() {
+    const aluno = document.getElementById("alunoifelse").value;
+    const nota = parseFloat(document.getElementById("notaifelse").value);
+
+    console.log(aluno, nota);
+
+    //processamento
+
+    let resp = null;
+
+    if (nota >= 6) {
+        resp = "Aprovado!"
+    } else if (nota < 4) {
+        resp = "Reprovado!"
+    } else {
+        resp = "Recuperação!"
+    }
+
+    //saída
+    document.write(
+        `Exemplo feito com IF ELSE <br>
+        Aluno: ${aluno} <br>
+        Nota: ${nota} <br>
+        Situação: ${resp}`);
+}
+
+function verificascase() {
+    const dia = parseFloat(document.getElementById("diasemana").value);
+
+    console.log(dia);
+
+    //processamento
+
+    let resp = null;
+
+    switch (dia) {
+        case 1:
+            resp = "Domingo!";
+            break;
+        case 2:
+            resp = "Segunda!";
+            break;
+        case 3:
+            resp = "Terça!";
+            break;
+        case 4:
+            resp = "Quarta!";
+            break;
+        case 5:
+            resp = "Quinta!";
+            break;
+        case 6:
+            resp = "Sexta!";
+            break;
+        case 7:
+            resp = "Sábado!";
+            break;
+
+        default:
+            document.write("Resposta não aceita!")
+        break;
+    }
+    console.log(resp);
+    //saída
+    document.write(
+        `Exemplo feito com SWITCH / CASE <br>
+        O dia digitado foi ${dia} e corresponde a ${resp}`);
+}
+
+
